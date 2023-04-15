f4eec386f78b9c43da80cc939245e8767d7b9d26
KR4SK <96661679+kr4sk@users.noreply.github.com>
2023-04-15 05:13:45 -0400

    Update SoundLoad.lua

--- SoundLoad.lua
+++ SoundLoad.lua
@@ -21,7 +21,7 @@ if not isfile("KrFiveDuels/song/ransom.wav") then
 		Text = "";
 		Icon = "";  
 	})
-    writefile("KrFiveDuels/song/ransom.wav", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/ransom.wav?raw=true")) -- https://www.youtube.com/watch?v=1XzY2ij_vL4
+    writefile("KrFiveDuels/song/ransom.wav", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/ransom.wav?raw=true"))
 end
 if not isfile("KrFiveDuels/song/Kurxxed.wav") then
     game.StarterGui:SetCore("SendNotification", {
@@ -29,7 +29,7 @@ if not isfile("KrFiveDuels/song/Kurxxed.wav") then
 		Text = "";
 		Icon = "";  
 	})
-    writefile("KrFiveDuels/song/Kurxxed.wav", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/Kurxxed.wav?raw=true")) -- https://www.youtube.com/watch?v=-DENhuNNkK4
+    writefile("KrFiveDuels/song/Kurxxed.wav", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/Kurxxed.wav?raw=true"))
 end
 if not isfile("KrFiveDuels/song/onthatline.mp3") then
     game.StarterGui:SetCore("SendNotification", {
@@ -37,5 +37,13 @@ if not isfile("KrFiveDuels/song/onthatline.mp3") then
 		Text = "";
 		Icon = "";  
 	})
-    writefile("KrFiveDuels/song/onthatline.mp3", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/onthatline.mp3?raw=true")) -- https://www.youtube.com/watch?v=-DENhuNNkK4
+    writefile("KrFiveDuels/song/onthatline.mp3", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/onthatline.mp3?raw=true"))
 end
+if not isfile("KrFiveDuels/song/LAM.mp3") then
+    game.StarterGui:SetCore("SendNotification", {
+		Title = "Creating LAM.mp3 song"; 
+		Text = "";
+		Icon = "";  
+	})
+    writefile("KrFiveDuels/song/LAM.mp3", game:HttpGet("https://github.com/kr4sk/FiveDuels/blob/main/LAM.mp3?raw=true"))
+	end
